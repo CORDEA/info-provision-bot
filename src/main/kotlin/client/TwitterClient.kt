b9092db4547fd7fa258834e1128aba01625219bd
Yoshihiro Tanaka <contact@cordea.jp>
2021-02-21 23:22:04 +0900

    add api client

---
+++ src/main/kotlin/client/TwitterClient.kt
@@ -0,0 +1,29 @@
+package client
+
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.features.*
+import io.ktor.client.request.*
+import java.io.Closeable
+
+class TwitterClient(token: String) : Closeable {
+    private companion object {
+        const val HOST = "https://api.twitter.com"
+        const val STREAM_RULES = "2/tweets/search/stream/rules"
+        const val STREAM = "2/tweets/search/stream"
+    }
+
+    private val client = HttpClient(CIO) {
+        defaultRequest {
+            host = HOST
+            header("Authorization", "Bearer $token")
+        }
+    }
+
+    fun postStreamRule() {
+    }
+
+    override fun close() {
+        client.close()
+    }
+}
