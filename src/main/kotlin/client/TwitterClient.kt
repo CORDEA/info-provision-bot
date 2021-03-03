9966da3294527adef0922178559e332399be621c
Yoshihiro Tanaka <contact@cordea.jp>
2021-03-03 21:55:54 +0900

    implement function to post stream rules

--- src/main/kotlin/client/TwitterClient.kt
+++ src/main/kotlin/client/TwitterClient.kt
@@ -6,27 +6,50 @@ import io.ktor.client.features.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
 import io.ktor.client.request.*
+import io.ktor.http.*
+import kotlinx.coroutines.flow.flow
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import java.io.Closeable
 
 class TwitterClient(token: String) : Closeable {
     private companion object {
-        const val HOST = "https://api.twitter.com"
+        const val HOST = "api.twitter.com"
         const val STREAM_RULES = "2/tweets/search/stream/rules"
         const val STREAM = "2/tweets/search/stream"
     }
 
+    private val json = Json {
+        ignoreUnknownKeys = true
+    }
+
     private val client = HttpClient(CIO) {
         defaultRequest {
-            host = HOST
+            url {
+                protocol = URLProtocol.HTTPS
+                host = HOST
+            }
             header("Authorization", "Bearer $token")
+            contentType(ContentType.Application.Json)
         }
 
         install(JsonFeature) {
-            serializer = KotlinxSerializer()
+            serializer = KotlinxSerializer(json)
         }
     }
 
-    fun postStreamRule() {
+    fun postStreamRules(rules: List<StreamRuleRequest>) = flow {
+        val response = client.post<String> {
+            url {
+                encodedPath = STREAM_RULES
+            }
+            body = StreamRulesRequest(rules)
+        }
+        emit(
+            runCatching {
+                json.decodeFromString<StreamRulesResponse.Success>(response)
+            }.getOrElse { json.decodeFromString<StreamRulesResponse.Error>(response) }
+        )
     }
 
     override fun close() {
