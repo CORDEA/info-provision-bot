779d6c35a6434805afca02f87a8064a540b36c41
Yoshihiro Tanaka <contact@cordea.jp>
2021-03-06 16:23:20 +0900

    observe regularly

--- src/main/kotlin/jp/cordea/ipbot/rss/observer.kt
+++ src/main/kotlin/jp/cordea/ipbot/rss/observer.kt
@@ -3,15 +3,24 @@ package jp.cordea.ipbot.rss
 import io.ktor.application.*
 import jp.cordea.ipbot.rss.client.RssClient
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlin.time.ExperimentalTime
 
+@ExperimentalTime
+@ExperimentalCoroutinesApi
 fun Application.observeRss() {
+    val interval = environment.config.property("rss.interval").getString().toLong()
     val urls = environment.config.property("rss.urls").getList()
     val client = RssClient()
 
-    client.getRss(urls)
+    flow {
+        while (true) {
+            emit(delay(interval))
+        }
+    }
+        .flatMapLatest { client.getRss(urls) }
         .flowOn(Dispatchers.IO)
         .onEach {
             // TODO
--- src/main/kotlin/jp/cordea/ipbot/server.kt
+++ src/main/kotlin/jp/cordea/ipbot/server.kt
@@ -3,7 +3,11 @@ package jp.cordea.ipbot
 import io.ktor.application.*
 import jp.cordea.ipbot.rss.observeRss
 import jp.cordea.ipbot.twitter.observeTweets
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlin.time.ExperimentalTime
 
+@ExperimentalTime
+@ExperimentalCoroutinesApi
 fun Application.main() {
     observeTweets()
     observeRss()
