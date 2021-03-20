3141a20d52c19e2e54977c9193102388ab27a9df
Yoshihiro Tanaka <contact@cordea.jp>
2021-03-20 14:00:07 +0900

    reimplement observer as class

---
+++ src/main/kotlin/jp/cordea/ipbot/rss/RssObserver.kt
@@ -0,0 +1,55 @@
+package jp.cordea.ipbot.rss
+
+import io.ktor.application.*
+import jp.cordea.ipbot.*
+import jp.cordea.ipbot.line.client.TextMessage
+import jp.cordea.ipbot.rss.client.RssItemResponse
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+import java.io.Closeable
+import kotlin.coroutines.CoroutineContext
+
+class RssObserver(
+    private val config: AppConfig,
+    private val application: Application,
+    private val registerFeedUseCase: RegisterFeedUseCase,
+    private val sendPushMessageUseCase: SendPushMessageUseCase,
+    private val getNewRssContentsUseCase: GetNewRssContentsUseCase,
+    private val getAuthenticatedUsersUseCase: GetAuthenticatedUsersUseCase
+) : CoroutineScope, Closeable {
+    private val job = SupervisorJob(application.coroutineContext[Job])
+
+    override val coroutineContext: CoroutineContext = application.coroutineContext + job
+
+    @ExperimentalCoroutinesApi
+    fun observe() {
+        val urls = config.rss.urls
+        registerFeedUseCase.execute(urls)
+        flow {
+            while (true) {
+                emit(delay(config.rss.interval))
+            }
+        }
+            .transform {
+                urls.map {
+                    emitAll(getNewRssContentsUseCase.execute(it))
+                }
+            }
+            .map { list -> format(list).map { TextMessage(it) } }
+            .flatMapLatest { messages ->
+                getAuthenticatedUsersUseCase.execute()
+                    .asFlow()
+                    .flatMapLatest { sendPushMessageUseCase.execute(it, messages) }
+            }
+            .flowOn(Dispatchers.IO)
+            .launchIn(this)
+    }
+
+    private fun format(items: List<RssItemResponse>) = items.map { format(it) }
+
+    private fun format(item: RssItemResponse) = item.title + "\n" + item.link
+
+    override fun close() {
+        job.cancelChildren()
+    }
+}
