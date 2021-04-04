bfcd86d3321c28431597d987fff59907300fdb4f
Yoshihiro Tanaka <contact@cordea.jp>
2021-04-04 14:05:10 +0900

    remove unnecessary config values

--- src/main/kotlin/jp/cordea/ipbot/AppConfig.kt
+++ src/main/kotlin/jp/cordea/ipbot/AppConfig.kt
@@ -3,26 +3,16 @@ package jp.cordea.ipbot
 import io.ktor.config.*
 
 class AppConfig(config: ApplicationConfig) {
-    val app = App(config.property("app.code").getString())
-    val twitter = Twitter(
-        config.property("twitter.token").getString(),
-        config.property("twitter.rules").getList()
-    )
+    val twitter = Twitter(config.property("twitter.rules").getList())
     val rss = Rss(
         config.property("rss.interval").getString().toLong(),
         config.property("rss.urls").getList()
     )
-    val line = Line(
-        config.property("line.secret").getString(),
-        config.property("line.token").getString()
-    )
     val googleCloud = GoogleCloud(config.property("googleCloud.projectId").getString())
     val db = Db(config.property("db.url").getString())
 
-    class App(val code: String)
-    class Twitter(val token: String, val rules: List<String>)
+    class Twitter(val rules: List<String>)
     class Rss(val interval: Long, val urls: List<String>)
-    class Line(val secret: String, val token: String)
     class GoogleCloud(val projectId: String)
     class Db(val url: String)
 }
